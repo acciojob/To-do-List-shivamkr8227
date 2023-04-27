5603088663e7ccf29cc50d042ec7b6888f74478d
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-27 22:06:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
-
+let nti=document.getElementById("newTodoInput");
+let sub=document.getElementById("addTodoBtn");
+ let tdl=document.getElementById("todoList");
+sub.addEventListener("click", function(){
+	let ntivalue=nti.value.trim();
+	if(ntivalue!=""){
+		let ne=document.createElement("li");
+		ne.textContent=ntivalue;
+		tdl.appendChild=ne;
+		nti.value="";
+	}
+});
